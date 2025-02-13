306b840228ac8b0fa9e9117c0bee6d9e2a79499a
ErickGaiero <erickgaiero15@gmail.com>
2025-02-13 20:28:20 +0000

    Ejercicio 5

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -3,8 +3,8 @@
 * main - Its the principal function
 * Return: Return zero
 */
-int main (void)
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+	return (0)
 }
