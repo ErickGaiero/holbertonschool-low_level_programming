e9a970ab066c163d84c74de75787cc397ba255d1
ErickGaiero <erickgaiero15@gmail.com>
2025-02-14 17:39:39 +0000

    Ejercicio 2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabetic
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	char l;
+
+	for (l = 'a'; l <= 'z'; l++)
+		putchar(l);
+
+	putchar('\n');
+	return (0);
+}
