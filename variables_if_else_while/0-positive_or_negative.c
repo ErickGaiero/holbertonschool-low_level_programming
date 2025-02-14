f6dc85298f5f69c48c68614648a9f657e6c96392
ErickGaiero <erickgaiero15@gmail.com>
2025-02-14 14:03:21 +0000

    Ejercicio 0

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,29 @@
-#include <stdio>
-
-int main (void)
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/**
+ * main - Funcion principal
+ *
+ * Return: Devuelve zero
+*/
+int main(void)
 {
-	
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n > 0)
+	{
+		printf(n "Is positive");
+	}
+	else if (n < 0)
+	{
+		printf(n "Is negative");
+	} else
+	{
+		printf("Is zero");
+	}
+	return (0);
 }
