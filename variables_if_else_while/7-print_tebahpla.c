17e2666d3e01ec39f6ccf4b95d0ec8b84d9f0ab6
ErickGaiero <erickgaiero15@gmail.com>
2025-02-17 13:07:51 +0000

    Ejercicio 7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in reverse
+ *
+ * Return: Success
+ */
+int main(void)
+{
+	char l;
+
+	for (l = 'z'; l <= 'a'; l--)
+		putchar(l);
+
+	putchar('\n');
+	return (0);
+}
