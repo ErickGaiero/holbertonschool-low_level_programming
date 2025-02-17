2755dc7aa6cb41b697aec9e6e3c8806990be2d7e
ErickGaiero <erickgaiero15@gmail.com>
2025-02-17 13:04:26 +0000

    Ejercicio 6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Is the principal function
+ *
+ * Return: Success
+ */
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+		putchar(n);
+
+	putchar('\n');
+	return (0);
+}
