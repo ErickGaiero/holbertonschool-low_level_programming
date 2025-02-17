20c1084840feca046745d52f8809a1f637670dbc
ErickGaiero <erickgaiero15@gmail.com>
2025-02-17 13:19:51 +0000

    Ejercicio 8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+  * main - Hexadecimal prints
+  *
+  * Return: Success
+  */
+int main(void)
+{
+	char l;
+
+	for (l = '0'; l <= '9'; l++)
+	{
+		putchar(l);
+	}
+
+	for (l = 'a'; l <= 'f'; l++)
+	{
+		putchar(l);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
