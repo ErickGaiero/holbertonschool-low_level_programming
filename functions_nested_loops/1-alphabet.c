530fe1b095a3d169252e5488629d1e36667df299
ErickGaiero <erickgaiero15@gmail.com>
2025-02-18 13:17:26 +0000

    Ejercicio 1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print the alphabet ten times
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+
+	_putchar('\n');
+}
---
+++ functions_nested_loops/main.h
@@ -0,0 +1,6 @@
+/**
+*File:main.h
+*Desc:function declaration
+*/
+int _putchar(char c);
+void print_alphabet(void);
