493a9b6803148cd4c326d092206c37a9d7b1a829
ErickGaiero <erickgaiero15@gmail.com>
2025-02-19 17:09:09 +0000

    Ejercicio 6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - Returns absolute numbers
+ * @a: Variable
+ * Return: absolute or number
+ */
+
+int _abs(int a)
+{
+	if (a < 0)
+	{
+		return ('a' * -1);
+	}
+	else
+	{
+		return ('a');
+	}
+}
