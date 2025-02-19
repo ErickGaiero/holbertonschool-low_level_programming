d010bb520d1a52d9be76a4ab12405b24af0cb7db
ErickGaiero <erickgaiero15@gmail.com>
2025-02-19 13:10:45 +0000

    Ejercicio 3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _islower - checks the lowercases
+ * @c: exist
+ * Return: 1 if lowercase, 0 Dosn't is lowercase
+ */
+
+int _islower(int c)
+{
+
+	if (c == 'a' && c <= 'z')
+	{
+		return (1);
+
+	}
+	else
+	{
+		return (0);
+	}
+}
