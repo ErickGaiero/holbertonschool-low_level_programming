a510d39ea5ba1c284bb841401ba1b6dc62553acb
ErickGaiero <erickgaiero15@gmail.com>
2025-02-24 16:44:59 +0000

    Ejercico 0

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,17 +1,18 @@
 #include "main.h"
 
 /**
- *
- *
+ *_isupper - Its the principal function
+ *@c: is a variable c
+ *Return: 1 its ok, 0 its lower
  */
 
 int _isupper(int c)
 {
 
 if (c <= 'Z' && c >= 'A')
-	{
-		return (1);
-	}
-		return (0);
+{
+	return (1);
+}
+	return (0);
 
 }
