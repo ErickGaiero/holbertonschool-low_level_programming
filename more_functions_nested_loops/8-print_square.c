0f006252a141e04766ad0496b569acc531269573
ErickGaiero <erickgaiero15@gmail.com>
2025-02-25 19:13:45 +0000

    Ejercicio 8

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_square - print square 
+ *
+ * @size: size
+ */
+
+void print_square(int size)
+
+{
+
+	int s = size;
+	
+	int i;
+
+	while (size > 0)
+	
+	{
+	
+		for (i = s; i > 0; i--)
+
+			_putchar ('#');
+		
+		if (size != 1)
+			
+			_putchar('\n');
+		
+		size--;
+	
+	}
+	
+	_putchar('\n');
+
+}
