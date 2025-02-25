0858914c2838a3dcb4cb78cc03a8e3bca0b6042c
ErickGaiero <erickgaiero15@gmail.com>
2025-02-25 13:01:29 +0000

    Ejercicio 4

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -10,15 +10,16 @@ void print_most_numbers(void)
 
 	for (numero = 0; numero <= 9; numero++)
 {
-	if (numero != 2 && numero != 4)
+		if (numero != 2 && numero != 4)
 
-        {
+        	{
 
-	_putchar(numero + '0');
-	
-	}
+		_putchar(numero + '0');
 
-	_putchar('\n');
+		_putchar('\n');
+
+		}
+
 	}
 
 }
