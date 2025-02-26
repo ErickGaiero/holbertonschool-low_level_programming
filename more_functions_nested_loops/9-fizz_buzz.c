bfdc38ec41232c3d4f8ea327814a511a668cd1b5
ErickGaiero <erickgaiero15@gmail.com>
2025-02-26 16:38:08 +0000

    Ejercicio 9

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,52 @@
+/**
+ * main - its the principal function
+ *
+ * Return: its ok
+ */
+
+int main(void)
+
+{
+	
+	int n;
+
+	for (n = 1; n <= 100; n++)
+
+	{
+
+		if (n % 5 == 0 && n % 3 == 0)
+	
+		{
+
+			printf("FizzBuzz");
+
+		} else if (n % 5 == 0)
+
+		{
+			printf("Buzz");
+
+		} else if (n % 3 == 0)
+
+		{
+
+			printf("Fizz");
+
+		} else
+
+		{
+
+			printf("%d", n);
+
+		}
+
+		if (n != 100)
+
+			printf(" ");
+
+	}
+
+	printf("\n");
+
+	return (0);
+
+}
