7163bfc6ee7f23961eb1de8383258209f2c29e59
ErickGaiero <erickgaiero15@gmail.com>
2025-02-26 16:57:53 +0000

    Ejercicio 9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -10,13 +10,14 @@ int main(void)
 {
 
 	int n;
+
 	for (n = 1; n <= 100; n++)
 	{
 		if (n % 5 == 0 && n % 3 == 0)
 		{
 			printf("FizzBuzz");
 		} else if (n % 5 == 0)
-		{		
+		{
 			printf("Buzz");
 		} else if (n % 3 == 0)
 		{
