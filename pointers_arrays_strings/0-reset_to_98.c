3b6d73ac90fedb3738e925196ac080a44a82e9d9
ErickGaiero <erickgaiero15@gmail.com>
2025-02-27 13:12:55 +0000

    Ejercicio 0

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *reset_to_98 - its the principal function
+ *
+ *@n: its the pointer that add the value to the variable
+ */
+
+void reset_to_98(int *n)
+{
+
+	*n = 98;
+
+}
