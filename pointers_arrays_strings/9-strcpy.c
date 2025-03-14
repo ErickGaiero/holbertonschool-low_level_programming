8f1fea324487b54615dfb5210e4da1902594b385
ErickGaiero <erickgaiero15@gmail.com>
2025-03-14 14:10:57 +0000

    Ejercicio 9

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include<stdlib.h>
 /**
  *_strcpy - its the principal function
  *@dest: site where save the copy of src
@@ -13,11 +13,12 @@ char *_strcpy(char *dest, char *src)
 	int i;
 
 	for (i = 0; src[i] != '\0'; i++)
-	{
+		{
+			dest[i] = src[i];
+		}
+	dest[i] = '\0';
 
-		src[i] = dest[i];
 
-	}
 
 	return (dest);
 }
