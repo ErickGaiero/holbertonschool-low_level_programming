08e07b894bf52769080cea7a4c4a0b2b875c23e9
ErickGaiero <erickgaiero15@gmail.com>
2025-03-17 12:25:41 +0000

    Ejercicio 2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -4,18 +4,19 @@
  * *_strcpy - its the principal function
  * @dest: destiny
  * @src: sourse
+ * @n: numbers uf chars
  * Return: Pointer
  */
 
-char *_strcpy(char *dest, char *src)
+char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; src[i] != '\0'; i++)
-	{
+	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
-	}
-	dest[i] = '\0';
+
+	for (; n > i; i++)
+		dest[i] = '\0';
 
 	return (dest);
 }
