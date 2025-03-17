433dd1ee8e3d8ff946bc078aa7e82314a77f3798
ErickGaiero <erickgaiero15@gmail.com>
2025-03-17 13:05:26 +0000

    Ejercicio 6

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+char *cap_string(char *s) {
+    int i = 0;
+    int c = 1;
+
+    char sep[] = " \t\n,;.!?\"(){}";
+
+    while (sep[i] != '\0') {
+        for (int j = 0; sep[j] != '\0'; j++) {
+            if (s[i] == sep[j]) {
+                c = 1;
+                break;
+            }
+        }
+
+        if (c && s[i] >= 'a' && s[i] <= 'z') {
+            s[i] -= 32;
+            c = 0;
+        } else if (s[i] >= 'A' && s[i] <= 'Z') {
+            c = 0;
+        }
+
+        i++;
+    }
+
+    return s;
+}
