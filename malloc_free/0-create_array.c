c0772675b81a550368f6079e39fb5620c5a5fb33
ErickGaiero <erickgaiero15@gmail.com>
2025-03-19 14:01:03 +0000

    Ejercicio 0

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,33 @@
+#include"main.h"
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * create_array - its the principal function
+ * @size: size of malloc
+ * @c: its the variable that char c
+ * Return: return j
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *j;
+
+	unsigned int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	pe = malloc(size * sizeof(*j));
+
+	if (j == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+		j[i] = c;
+
+	return (j);
+}
