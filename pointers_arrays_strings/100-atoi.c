f051657469a258c2afec4d37cb763134e1aee165
ErickGaiero <erickgaiero15@gmail.com>
2025-04-20 23:42:05 +0000

    Exercise 100 finished

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,35 +1,36 @@
 #include "main.h"
 
 /**
- * _atoi - Its the principal function.
- * @s: String
- * Return: String result.
+ * _atoi - Its the principal function
+ * @s: string
+ * Return: Result
  */
 
 int _atoi(char *s)
 {
-	int index = 0;
+	int i = 0;
 	int sign = 1;
 	int result = 0;
 	int started = 0;
 
-	while (s[index] != '\0')
+	while (s[i] != '\0')
 	{
-		if (s[index] == '-')
+		if (s[i] == '-' && !started)
 		{
 			sign *= -1;
 		}
-		else if (s[index] >= '0' && s[index] <= '9')
+		else if (s[i] >= '0' && s[i] <= '9')
 		{
-			result = result * 10 + (s[index] - '0');
+			result = result * 10 + (s[i] - '0');
 			started = 1;
 		}
 		else if (started)
 		{
-		break;
+			break;
 		}
-		index++;
+		i++;
 	}
 
-	return (result * sign);
+    return result * sign;
 }
+
