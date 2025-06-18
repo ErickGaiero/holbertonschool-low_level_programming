c19cb83ce83160994e18090ab6338cafceebeb09
ErickGaiero <erickgaiero15@gmail.com>
2025-06-18 14:55:40 +0000

    Exercise 5

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -5,10 +5,10 @@
  * Return: Return zero
  */
 
-int main (void)
+int main(void)
 {
 
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
 	return (0);
 }
