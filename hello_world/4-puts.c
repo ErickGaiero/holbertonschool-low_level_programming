fa291a110b8bc1cea606330b395e4b9e42d2eabe
ErickGaiero <erickgaiero15@gmail.com>
2025-06-18 14:47:04 +0000

    Exercise 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 
-int main(){
+/**
+ * main - It's the principal function
+ * Return: Return zero
+ */
 
-char str [] = "\"Programming is like building a multilingual puzzle";
+int main(void)
+{
 
-puts (str);
+	char str[] = "\"Programming is like building a multilingual puzzle";
 
-return 0;
+	puts(str);
+
+return (0);
 }
