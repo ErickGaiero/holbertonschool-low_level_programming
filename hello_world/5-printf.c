ae30b70b1fb86c2610e46ea62fc15905a937ed1f
ErickGaiero <erickgaiero15@gmail.com>
2025-06-18 14:52:58 +0000

    Exercise 5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - It's the principal function
+ * Return: Return zero
+ */
+
+int main (void)
+{
+
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}
