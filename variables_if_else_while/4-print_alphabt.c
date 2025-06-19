de48083c650e94d50b9bf0bd2a931b288f4533cc
ErickGaiero <erickgaiero15@gmail.com>
2025-06-19 14:26:54 +0000

    Exercise 4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *main - It's the principal function
+ *alphabet: Its the variable of alphabet
+ *Return: Return zero
+ */
+
+int main(void)
+{
+
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++){
+		if(alphabet != 'q' && alphabet != 'e')
+		putchar(alphabet);
+	}
+	putchar('\n');
+
+	return (0);
+}
