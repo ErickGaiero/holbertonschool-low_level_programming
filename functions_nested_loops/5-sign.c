20cdf4305f20de2f8aa0955d786a40b0a9881736
ErickGaiero <erickgaiero15@gmail.com>
2025-06-23 18:56:36 +0000

    Exercise 5

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - Print the sign of the number ingresed
+ * @n: Variable of number
+ * Return: 1 is positive, 0 is zero and -1 is negative
+ */
+
+int print_sign(int n)
+{
+
+	if(n > 0)
+	{
+		_putchar('+');
+		return(1);
+	}
+	else if(n < 0)
+	{
+		_putchar('-');
+		return(-1);
+	}
+	else
+	{
+		_putchar('0');
+		return(0);
+	}
+}
