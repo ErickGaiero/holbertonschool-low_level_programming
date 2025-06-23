6b44764202151cd03e0a222a7954cad5935d8bd0
ErickGaiero <erickgaiero15@gmail.com>
2025-06-23 17:02:10 +0000

    Exercise 3

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
 /**
- *_islower - If mayus return 0 else return 1
- *@c: Character
+ * _islower - Print 1 and 0 to lower or upper
+ * @c: Character
+ * Return: return 1 if is mayus return 0 if is lower
  */
 
 int _islower(int c)
 {
 
-	if(c >= 'a' && c <= 'z')
+	if (c >= 'a' && c <= 'z')
 	{
 		return (1);
 	}
