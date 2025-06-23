de2c601d53a8b182d7c1579fe05c2b30a9815f68
ErickGaiero <erickgaiero15@gmail.com>
2025-06-23 19:06:23 +0000

    Exercise 6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs - Print the absolute value of a number
+ * @r: Number ingresed
+ * Return: -r if is negative, r if another case
+ */
+
+int _abs(int r)
+{
+
+	if (r < 0)
+	{
+		return (-r);
+	}
+	else
+	{
+		return (r);
+	}
+
+}
