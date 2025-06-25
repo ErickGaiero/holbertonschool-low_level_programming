4fa68f7edd768223f75069d29dc84a2fddae29be
ErickGaiero <erickgaiero15@gmail.com>
2025-06-25 00:43:00 +0000

    Exercise 10

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -2,12 +2,12 @@
 
 /**
  *add - Add the numbers ingresed
- *
+ *@n0: First parameter
+ *@n1: Second parameter
+ *Return: Return the add of n0 and n1
  */
 
 int add(int n0, int n1)
 {
-
-	return(n0 + n1);
-
+	return (n0 + n1);
 }
