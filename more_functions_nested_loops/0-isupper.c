afd68512d191453bb60039b326e8e42ee05344f4
ErickGaiero <erickgaiero15@gmail.com>
2025-06-25 17:10:31 +0000

    Exercise 0

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isupper - Print 1 and 0 to upper or another
+ * @c: Character
+ * Return: return 1 if is upper return 0 if is another
+ */
+
+int _isupper(int c)
+{
+
+	if (c >= 'A' && c <= 'A')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
