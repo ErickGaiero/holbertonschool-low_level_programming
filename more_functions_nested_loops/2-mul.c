8cce479de8199f7729d6e78858d64b2dde3d1442
ErickGaiero <erickgaiero15@gmail.com>
2025-06-26 16:45:48 +0000

    Exercise 2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *mul - Multiplicates a and b
+ *@a: First number
+ *@b: Second number
+ *Return: a multiplicate b
+ */
+
+int mul(int a, int b)
+{
+
+return (a * b);
+
+}
