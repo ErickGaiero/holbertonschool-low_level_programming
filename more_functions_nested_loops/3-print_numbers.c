afc1032bd4e1b2b5159ef0c6c7b0d2c8eb4e109b
ErickGaiero <erickgaiero15@gmail.com>
2025-06-26 17:01:58 +0000

    Exercise 3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *print_numbers - Print numbers of the 0 to 9
+ *
+ */
+
+void print_numbers(void)
+{
+
+	int number;
+
+	for(number = '0'; number <= '9'; number++)
+	{
+
+		_putchar(number);
+
+	}
+
+	_putchar('\n');
+}
