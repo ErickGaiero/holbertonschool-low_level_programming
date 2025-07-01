51a136b619e211e808cadc6ee716ca32739f1a18
ErickGaiero <erickgaiero15@gmail.com>
2025-07-01 23:10:33 +0000

    Exercise 3

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -2,13 +2,19 @@
 #include "main.h"
 
 /**
- *
- *
+ *_puts - Print a String
+ *@str: string
  */
 
 void _puts(char *str)
 {
 
+	int i;
 
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
 
 }
