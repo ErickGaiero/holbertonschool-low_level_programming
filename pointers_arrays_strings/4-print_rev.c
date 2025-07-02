ef89119b878ad153bd142e90bc3b7ecff4d62e8d
ErickGaiero <erickgaiero15@gmail.com>
2025-07-02 13:18:59 +0000

    Exercise 4

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -11,18 +11,18 @@ void print_rev(char *s)
 {
 
 
-	int i, length;
+	int i;
 
 	for (i = 0; s[i]; i++)
 	{
-		length++;
+		continue;
 	}
-
-	for (i = length - 1; i != 0; i--)
+	i -= 1;
+	for (; i >= 0; i--)
 	{
 
 		_putchar(s[i]);
 
-	}
+	}	
 	_putchar('\n');
 }
