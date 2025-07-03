5ccbe3848e864132f88dbe243348f71a702cc04e
ErickGaiero <erickgaiero15@gmail.com>
2025-07-03 18:27:32 +0000

    Exercise 5

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *
+ *
+ */
+
+void rev_string(char *s)
+{
+
+	int i, j = 0;
+	char a[1000];
+
+	for (i = 0; s[i]; i++)
+	{
+		;
+	}
+
+	i -= 1;
+
+	for (; s[i]; i--)
+	{
+		a[j] = s[i];
+		j++;
+	}
+	for (i = 0; s[i]; i++)
+	{
+		s[i] = a[i];
+	}
+}
