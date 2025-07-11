ce4219dc903f6efa7cc4637b7da79a2f76b92282
ErickGaiero <erickgaiero15@gmail.com>
2025-07-11 18:05:53 +0000

    Exercise 5

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *_strstr - Find and print the coincidence
+ *@haystack: Parameter
+ *@needle: Another parameter
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+	int s = 0;
+
+	while (needle[s] != '\0')
+		s++;
+
+	while (*haystack)
+	{
+		for (i = 0; needle[i] != '\0'; i++)
+		{
+			if (haystack[i] != needle[i])
+			{
+				break;
+			}
+		}
+		if (i != s)
+		{
+			haystack++;
+		}
+		else
+		{
+			return (haystack);
+		}
+	}
+	return (0);
+}
