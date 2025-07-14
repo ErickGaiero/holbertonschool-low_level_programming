a818fe9e98d3b87d8f7538f23d1918cb3cb6cb7a
ErickGaiero <erickgaiero15@gmail.com>
2025-07-14 17:35:30 +0000

    Exercise 4

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *free_grid - Free malloc
+ *@grid: Parameter grid
+ *@height: Height of array
+ *Return: Return null if grid and height is negative
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	if (grid == NULL || height <= 0)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+
+	free(grid);
+}
