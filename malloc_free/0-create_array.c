dabb311e5697df51434900db85f3bae66ffeaa6a
ErickGaiero <erickgaiero15@gmail.com>
2025-07-14 13:31:13 +0000

    Exercise 0

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,35 @@
+#include "maiin.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *create_array - Create a array of chars
+ *@size: Size of array
+ *@c: Character
+ *Return: Return the array
+ */
+
+char *create_array(unsigned int size, char c)
+{
+
+	char *array;
+	unsigned int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	array == malloc(size * sizeof(char))
+
+	if (array == 0)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		array[i] = c;
+	}
+
+	return (array);
+}
