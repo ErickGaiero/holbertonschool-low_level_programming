30da8b958b2ca7dc5ed0949e1468c76d2dd41df9
ErickGaiero <erickgaiero15@gmail.com>
2025-07-14 16:12:20 +0000

    Exercise 2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -3,8 +3,10 @@
 #include <stdlib.h>
 
 /**
- *
- *
+ *str_concat - Concatenate 2 strings
+ *@s1: String 1
+ *@s2: String 2
+ *Return: Final string
  */
 
 char *str_concat(char *s1, char *s2)
@@ -13,25 +15,28 @@ char *str_concat(char *s1, char *s2)
 	char *a;
 	int i, j, len1, len2;
 
-	for (len1 = 0; s1[len1] != '\0'; len1++)
+	if (s1 != '\0')
 	{
-		continue;
-	}
-	for (len2 = 0; s2[len2] != '\0'; len2++)
-	{
-		continue;
+		for (len1 = 0; s1[len1] != '\0'; len1++)
+		{
+			continue;
+		}
 	}
 
-	a = malloc (sizeof(char) * (len1 + len2 + 1));
+	if (s2 != '\0')
+	{
+		for (len2 = 0; s2[len2] != '\0'; len2++)
+		{
+			continue;
+		}
+	}
 
-	if (s1 == NULL)
+	a = malloc(sizeof(char) * (len1 + len2 + 1));
+
+	if (a == NULL)
 	{
 		return (NULL);
 	}
-	if (s2 == NULL)
-        {
-                return (NULL);
-        }
 
 	for (i = 0; i < len1; i++)
 	{
