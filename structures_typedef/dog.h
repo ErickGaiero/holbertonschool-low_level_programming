db8741c6cc1fcc2707aeb390321785168b385746
ErickGaiero <erickgaiero15@gmail.com>
2025-07-17 19:41:30 +0000

    Betty

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ *struct dog - Astruct that exercise zero
+ *@name: Name of the dog
+ *@age: Age of dog
+ *@owner: Owner of the do
+ */
+
 struct dog
 {
 	char *name;
