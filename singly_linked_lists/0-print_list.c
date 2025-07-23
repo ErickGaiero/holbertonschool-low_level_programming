3eb87df14c1f448eaa8e5eb200a26b7afd17eedd
ErickGaiero <erickgaiero15@gmail.com>
2025-07-23 15:30:24 +0000

    Exercise 0

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -10,22 +10,22 @@
 
 size_t print_list(const list_t *h)
 {
-    size_t count = 0;
+
+	size_t count = 0;
 
 	while (h != NULL)
 	{
-		if (h->str == NULL)
-		{
-			printf("[0] (nil)\n");
-		}
-		else
-		{
- 			printf("[%u] %s\n", h->len, h->str);
-		}
-
-		h = h->next;
-		count++;
-	
+	if (h->str == NULL)
+	{
+		printf("[0] (nil)\n");
 	}
-	return count;
+	else
+	{
+		printf("[%u] %s\n", h->len, h->str);
+	}
+
+	h = h->next;
+	count++;
+	}
+	return (count);
 }
