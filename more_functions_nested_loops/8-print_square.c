9b382fa76dbf718f444fc01b581d8a69c4429b34
ErickGaiero <erickgaiero15@gmail.com>
2025-07-28 17:27:54 +0000

    Exercise 8

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_square - Print a square
+ * @size: Size of square
+ * Return: If size is negative return null
+ */
+
+void print_square(int size)
+{
+	int row, col;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (row = 0; row < size; row++)
+	{
+		for (col = 0; col < size; col++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
