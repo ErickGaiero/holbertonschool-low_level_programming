204705b83149ba5f33def6f076150486cc453c46
ErickGaiero <erickgaiero15@gmail.com>
2025-07-28 14:34:21 +0000

    Exercise 5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *
+ *
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j >= 10)
+			{
+				_putchar('1');
+			}
+
+				_putchar('0' + (j % 10));
+
+		}
+		_putchar('\n');
+	}
+}
