bd73bbbea233a8e4ff9e49f421b18630d1ac7338
ErickGaiero <erickgaiero15@gmail.com>
2025-07-28 17:03:03 +0000

    Exercise 6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*print_line - Prints a straight line
+*@n:Number of lines
+*/
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
