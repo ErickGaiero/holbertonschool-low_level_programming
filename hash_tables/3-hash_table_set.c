4536756f5a5cefa76a8fa6541d082e0438046dbc
ErickGaiero <erickgaiero15@gmail.com>
2025-08-04 14:42:55 +0000

    Exericse 3

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -20,14 +20,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
-	
+
 	index = key_index((const unsigned char *)key, ht->size);
-
 	actual = ht->array[index];
-
 	while (actual)
 	{
-
 		if (strcmp(actual->key, key) == 0)
 		{
 			new_value = strdup(value);
@@ -35,15 +32,14 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 				return (0);
 
 			free(actual->value);
-			actual -> value = new_value;
+			actual->value = new_value;
 			return (1);
 		}
-		actual = actual->next; 
+		actual = actual->next;
 	}
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 		return (0);
-
 	new_node->key = strdup(key);
 	new_node->value = strdup(value);
 
@@ -52,7 +48,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		free(new_node->key);
 		free(new_node->value);
 		free(new_node);
-		return 0;
+		return (0);
 	}
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
