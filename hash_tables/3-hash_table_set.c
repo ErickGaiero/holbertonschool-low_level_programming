1d1571f718017b4a1d6aa89b37e929fc52183acb
ErickGaiero <erickgaiero15@gmail.com>
2025-08-04 14:31:32 +0000

    Exericse 3

---
+++ hash_tables/3-hash_table_set.c
@@ -0,0 +1,60 @@
+#include "hash_tables.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * hash_table_set - Add a one elemnt to the hash table
+ * @ht: Hash table
+ * @key: Key
+ * @value: Value of key
+ * Return: True or false, OK or wrong
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+
+	unsigned long int index;
+	hash_node_t *new_node, *actual;
+	char *new_value;
+
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+		return (0);
+	
+	index = key_index((const unsigned char *)key, ht->size);
+
+	actual = ht->array[index];
+
+	while (actual)
+	{
+
+		if (strcmp(actual->key, key) == 0)
+		{
+			new_value = strdup(value);
+			if (new_value == NULL)
+				return (0);
+
+			free(actual->value);
+			actual -> value = new_value;
+			return (1);
+		}
+		actual = actual->next; 
+	}
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (0);
+
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+
+	if (new_node->key == NULL || new_node->value == NULL)
+	{
+		free(new_node->key);
+		free(new_node->value);
+		free(new_node);
+		return 0;
+	}
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+	return (1);
+}
