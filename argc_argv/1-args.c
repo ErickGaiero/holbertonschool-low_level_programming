e0e820c000722b35ca9589a0fffdee5ba44f8957
ErickGaiero <erickgaiero15@gmail.com>
2025-08-14 23:11:02 +0000

    Exercise 1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * 1argc - Main function
+ *@argc: Number of arguments
+ *@argv: Array of argument
+ *Return: 0
+ */
+
+int 1argc(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
