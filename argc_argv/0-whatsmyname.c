563d32450155c740300db2d18b0da5443e9b9be1
ErickGaiero <erickgaiero15@gmail.com>
2025-08-14 22:59:45 +0000

    Exercise 0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the program name
+ * @argc: Number of arguments
+ * @argv: Array of argument
+ * Return: 0 
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return 0;
+}
