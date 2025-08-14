3af38d0a6edcc33f0eee5abcc15fcd5a1c6f491c
ErickGaiero <erickgaiero15@gmail.com>
2025-08-14 00:05:41 +0000

    Exercise 4

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _pow_recursion - Value of x raised to the power of y
+ * @x: Base
+ * @y: Exponent
+ * Return: x^y
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+	return (x * _pow_recursion(x, y - 1));
+}
